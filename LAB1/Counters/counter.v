4caad485e08fd05f96a1755fb35113486a9608e2
Худяков Артём Сергеевич <xyarce@mail.ru>
2023-04-11 11:58:31 +0300

    progress...

---
+++ LAB1/Counters/counter.v
@@ -0,0 +1,13 @@
+module counter(clk, reset, divided_clk);
+	input clk;
+	input reset;
+	output reg [26:0]divided_clk;
+	
+	always @(posedge clk)
+	begin
+		if (reset == 0)
+			divided_clk = 0;
+		else
+			divided_clk <= divided_clk + 1'd1;
+	end
+endmodule	
